bbbe0c82547e6c7ca1727a001fb02ec51b73f129
lanemunds <lanemunds@gmail.com>
2023-04-22 11:05:34 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get('https://espn.com')
