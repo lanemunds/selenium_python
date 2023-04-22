4cb4cc6a8ec00aef49b1d8a1655f847deb3cf53e
lanemunds <lanemunds@gmail.com>
2023-04-22 13:04:25 -0600

    send keys

--- second.py
+++ second.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 service_obj = Service('/Users/lanemunds/Downloads/chromedriver')
 
 driver = webdriver.Chrome(service=service_obj)
-
 driver.get('https://espn.com')
-driver.get('https://cougarboard.com')
 driver.maximize_window()
-driver.back()
-print(driver.title)
-print(driver.current_url)
 driver.get('https://google.com')
-driver.find_element(By.NAME, 'Q').send_keys('Ivory Edmunds')
-
-driver.close()
+driver.find_element(By.NAME, 'q').send_keys('Ivory Edmunds')
+driver.find_element(By.NAME, 'q').send_keys(Keys.RETURN)
+driver.find_element(By.CLASS_NAME , 'byrV5b' ).click()
+time.sleep(5)
+driver.back()
+driver.back()
+driver.find_element(By.NAME, 'q').send_keys('Finnley Edmunds')
+driver.find_element(By.NAME, 'q').send_keys(Keys.RETURN)
+driver.find_element(By.CLASS_NAME , 'byrV5b' ).click()
+time.sleep(5)
+driver.get('https://lanemunds.github.io/')
+driver.find_element(By.ID, '').click()
+time.sleep(5)
