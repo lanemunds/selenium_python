5b4a7f777e28439486a32c26dc82efef2ca57c6e
lanemunds <lanemunds@gmail.com>
2023-04-22 12:19:32 -0600

    element

--- second.py
+++ second.py
@@ -6,3 +6,12 @@ service_obj = Service('/Users/lanemunds/Downloads/chromedriver')
 driver = webdriver.Chrome(service=service_obj)
 
 driver.get('https://espn.com')
+driver.get('https://cougarboard.com')
+driver.maximize_window()
+driver.back()
+print(driver.title)
+print(driver.current_url)
+driver.get('https://google.com')
+driver.find_element(By.NAME, 'Q').send_keys('Ivory Edmunds')
+
+driver.close()
